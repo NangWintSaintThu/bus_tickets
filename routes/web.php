73d90920ceeac42562f3d86af8e20dbf16004bad
Nang Wint Saint Thu <nangwintsaintthu@gmail.com>
2020-09-11 14:07:24 +0700

    edit by nang wint saint thu

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PageController@homefun')->name('homepage');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
